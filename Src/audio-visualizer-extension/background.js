c202aba3e5603ad185189d67581b8789e2831070
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-06-05 02:01:16 +0200

    Fix extension

---
+++ Src/audio-visualizer-extension/background.js
@@ -0,0 +1,24 @@
+
+// function that injects code to a specific tab
+function injectScript(tabId) {
+
+    chrome.scripting.executeScript(
+        {
+            target: {tabId: tabId},
+            files: ['inject.js'],
+        }
+    );
+
+}
+
+// adds a listener to tab change
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+    // check for a URL in the changeInfo parameter (url is only added when it is changed)
+    if (changeInfo.url.startsWith('https://open.spotify.com')) {
+        
+        // calls the inject function
+        injectScript(tabId);
+
+    }
+});
